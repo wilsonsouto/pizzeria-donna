8c005cd65a9508ba221ee39aead3966ea6aea952
Wilson Souto <wilsonsdr@outlook.com.br>
2024-04-11 23:33:03 -0300

    Removing property text-center in heading component

--- src/components/Elements/Heading.tsx
+++ src/components/Elements/Heading.tsx
@@ -8,7 +8,7 @@ interface HeadingProps {
 export default function Heading(props: HeadingProps) {
   return (
     <h2
-      className={`text-center text-4xl ${props.color} font-header sm:text-4xl md:text-4xl lg:text-4xl xl:text-4xl 2xl:text-5xl`}
+      className={`text-4xl ${props.color} font-header sm:text-4xl md:text-4xl lg:text-4xl xl:text-4xl 2xl:text-5xl`}
     >
       {props.text}
     </h2>
