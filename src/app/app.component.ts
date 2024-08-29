999fcb9f8a1bf0062e4cdf64dc18953ee2129bd9
Wilson Souto <wilsonsouto@outlook.com.br>
2024-08-29 15:44:24 -0300

    feat: add banner component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BannerComponent } from './banner/banner.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BannerComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'pizzeria-donna';
-}
+export class AppComponent {}
--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 
+interface Props {
+  value1: string;
+  value2: string;
+}
+
 @Component({
   selector: 'app-banner',
   standalone: true,
   imports: [],
   templateUrl: './banner.component.html',
-  styleUrl: './banner.component.scss'
+  styleUrl: './banner.component.scss',
 })
-export class BannerComponent {
-
+export class BannerComponent implements Props {
+  value1 = 'Pizzeria';
+  value2 = 'Donna';
 }
