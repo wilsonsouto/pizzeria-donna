dbf412678f5cca693b617aeee984f327adf12716
Wilson Souto <wilsonsouto@outlook.com.br>
2024-08-31 20:33:41 -0300

    feat: add service component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { BannerComponent } from './banner/banner.component';
+import { ServiceComponent } from './service/service.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, BannerComponent],
+  imports: [BannerComponent, ServiceComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
---
+++ src/app/service/service.component.ts
@@ -0,0 +1,30 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+interface Props {
+  heading: string[];
+  subheading: string;
+  services: string[];
+}
+
+@Component({
+  selector: 'app-service',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './service.component.html',
+  styleUrl: './service.component.scss',
+})
+export class ServiceComponent implements Props {
+  heading: string[] = [
+    'United just for one reason',
+    "Don't be afraid, the hungry does not have to wait",
+  ];
+  subheading: string =
+    'We are the first pizzeria to focus on this kind of service. Contributing to a better experience, security, comfort and quality. To place an order, simply use our chatbot!';
+  services: string[] = [
+    '✓ First quality ingredients',
+    '✓ Inspired recipes',
+    '✓ Love in every detail',
+    '✓ Our commitment is to always be present and meet your needs',
+  ];
+}
